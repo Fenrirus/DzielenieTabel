761849a4e43e72f5fae7ce586d764aa0416d9f91
Marian Marian <fenrir55@gmail.com>
2018-11-19 20:22:49 +0100

    wersja 1

---
+++ DzielenieTabel/WebForm1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DzielenieTabel.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged">
+                <asp:ListItem Value="All">Load all Employees</asp:ListItem>
+                <asp:ListItem Value="Pemanent">Load Permanent Employees</asp:ListItem>
+                <asp:ListItem Value="Contract">Load Contract Employees</asp:ListItem>
+            </asp:RadioButtonList>
+            <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+                <AlternatingRowStyle BackColor="White" />
+                <EditRowStyle BackColor="#7C6F57" />
+                <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#E3EAEB" />
+                <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F8FAFA" />
+                <SortedAscendingHeaderStyle BackColor="#246B61" />
+                <SortedDescendingCellStyle BackColor="#D4DFE1" />
+                <SortedDescendingHeaderStyle BackColor="#15524A" />
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DzielenieTabel/WebForm1.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DzielenieTabel
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        private DataTable convertEmployeeForDisplay(List<Employees2> employees2s)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Gender");
+            dt.Columns.Add("AnuualSalary");
+            dt.Columns.Add("HourlyPay");
+            dt.Columns.Add("HoursWorked");
+            dt.Columns.Add("Type");
+
+            foreach(Employees2 person in employees2s)
+            {
+                DataRow dr = dt.NewRow();
+                dr["ID"] = person.ID;
+                dr["Name"] = person.Name;
+                dr["Gender"] = person.Gender;
+                if (person is Permament)
+                {
+                    dr["AnuualSalary"] = ((Permament)person).AnuualSalary;
+                    dr["Type"] = "Permament";
+                }
+                else
+                {
+                    dr["HourlyPay"] = ((Contract)person).HourlyPay;
+                    dr["HoursWorked"] = ((Contract)person).HoursWorked;
+                    dr["Type"] = "Contract";
+                }
+                dt.Rows.Add(dr);
+            }
+            return dt;
+        }
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SampleDataContext sdc = new SampleDataContext();
+            sdc.Log = Response.Output;
+            switch (RadioButtonList1.SelectedValue)
+            {
+                case "Pemanent":
+                    GridView1.DataSource = sdc.Employees2.OfType<Permament>().ToList();
+                    GridView1.DataBind();
+                    break;
+                case "Contract":
+                    GridView1.DataSource = sdc.Employees2.OfType<Contract>().ToList();
+                    GridView1.DataBind();
+                    break;
+                default:
+                    GridView1.DataSource = convertEmployeeForDisplay(sdc.Employees2.ToList());
+                    GridView1.DataBind();
+                    break;
+            }
+        }
+    }
+}
